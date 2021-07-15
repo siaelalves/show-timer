1875fda15a957f65811b6e77e9e231f473c0b091
Siael Alves <32615727+siaelalves@users.noreply.github.com>
2021-07-15 12:48:23 -0300

    Upload inicial

---
+++ ShowTimer/main.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblTm = New System.Windows.Forms.Label()
+        Me.tm = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblTm
+        '
+        Me.lblTm.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblTm.BackColor = System.Drawing.Color.White
+        Me.lblTm.Font = New System.Drawing.Font("Arial", 144.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTm.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblTm.Location = New System.Drawing.Point(0, 0)
+        Me.lblTm.Name = "lblTm"
+        Me.lblTm.Size = New System.Drawing.Size(908, 264)
+        Me.lblTm.TabIndex = 0
+        Me.lblTm.Text = "Tempo"
+        Me.lblTm.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'tm
+        '
+        Me.tm.Enabled = True
+        Me.tm.Interval = 1000
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(909, 266)
+        Me.Controls.Add(Me.lblTm)
+        Me.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.KeyPreview = True
+        Me.Name = "main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "S H O W    T I M E R"
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.Color.White
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblTm As System.Windows.Forms.Label
+    Friend WithEvents tm As System.Windows.Forms.Timer
+
+End Class
---
+++ ShowTimer/main.vb
@@ -0,0 +1,38 @@
+﻿Public Class main
+
+    Dim startmove As Boolean = False
+
+    Private Sub tm_Tick(sender As Object, e As EventArgs) Handles tm.Tick
+        lblTm.Text = Now.ToString("HH:mm:ss")
+
+    End Sub
+
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblTm.Text = Now.ToString("HH:mm:ss")
+    End Sub
+
+    Private Sub lblTm_Click(sender As Object, e As EventArgs) Handles lblTm.Click
+        If startmove = True Then
+            startmove = False
+            Return
+        Else
+            startmove = True
+        End If
+
+    End Sub
+
+    Private Sub lblTm_MouseMove(sender As Object, e As MouseEventArgs) Handles lblTm.MouseMove
+        'If startmove = True Then
+        '    Me.Location = New System.Drawing.Point(CInt(MousePosition.X + (Me.Size.Width / 2)),
+        '                                           CInt(MousePosition.Y + (Me.Size.Height / 2)))
+        'End If
+    End Sub
+
+    Private Sub main_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        'Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+    End Sub
+
+    Private Sub lblTm_DoubleClick(sender As Object, e As EventArgs) Handles lblTm.DoubleClick
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.Sizable
+    End Sub
+End Class
